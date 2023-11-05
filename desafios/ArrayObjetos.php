1b77443dbe0a6c4f483def17e7695c75caf617e0
moises.neto <moises.neto@animaeducacao.com.br>
2023-11-05 20:24:31 -0300

    array com objeto

---
+++ desafios/ArrayObjetos.php
@@ -0,0 +1,9 @@
+<?php
+
+$pessoas = array(
+ array("nome" => "maria", "estado" => "RJ"),
+ array("nome" => "jose" , "estado" => "sp", "idade" => 25)
+);
+echo($pessoas[0]["estado"] . "\n") ;
+echo($pessoas[1]["idade"]) ;
+
